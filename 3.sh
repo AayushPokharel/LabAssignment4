27c8ff7298f8aef2be5f0f223522c73d76dcb4e7
AayushPokharel <40122245+AayushPokharel@users.noreply.github.com>
2022-09-13 16:39:51 +0545

    Update 3.sh

--- 3.sh
+++ 3.sh
@@ -1,1 +1,22 @@
 #!/bin/bash
+
+#sudo apt install ncal
+
+echo i.
+cal -m Jan 2000
+echo
+
+echo ii.
+cal -m Feb 1999
+echo
+
+echo iii.
+cal -m 9 7
+echo
+
+echo iv.
+cal
+echo
+
+echo v.
+ncal -H 2022-09-13
